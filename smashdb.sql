74ba18a1887bb28a7ae5e8e0c35ee3dda643ba19
nicksettoon <nwsettoon+github@gmail.com>
2020-04-22 19:06:23 -0500

    added query ideas.

--- smashdb.sql
+++ smashdb.sql
@@ -29,9 +29,16 @@ main_char_id    INTEGER,
 region  INTEGER
 );
 
+CREATE TABLE PlayerTournament
+(
+    plyr_id INTEGER,
+    tnmt_id INTEGER
+);
+
 CREATE TABLE Tournament
 (
 tnmt_id INTEGER,
+tname   VARCHAR(50),
 start_date   DATE,
 end_date     DATE,
 region      VARCHAR(50),
@@ -41,7 +48,28 @@ winner      VARCHAR(50)
 CREATE TABLE Character
 (
 cname VARCHAR(17),
+max_air_acceleration    FLOAT,
+air_speed   FLOAT,
+fall_speed  FLOAT,
+gravity FLOAT,
+full_jump_height    FLOAT,
+weight  FLOAT,
+walk_speed  FLOAT,
+init_dash   FLOAT,
+run_speed   FLOAT,
+fastest_OOS_option  FLOAT,
+grab_speed  FLOAT,
+total_wins INTEGER,
+tier INTEGER
 );
 
 CREATE TABLE Matchup
 (
+c1_id INTEGER,
+c2_id INTEGER,
+c1_wins     INTEGER,
+c2_wins     INTEGER,
+total_games INTEGER,
+tier_diff INTEGER
+);
+
