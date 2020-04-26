dee57682a7893a1b085349330f3a526c5ef9e8b8
nicksettoon <nwsettoon+github@gmail.com>
2020-04-26 11:17:29 -0500

    Set wins query done.

--- queries.sql
+++ queries.sql
@@ -4,7 +4,6 @@ SELECT pname as Player, COUNT(*) as Tourneys
     GROUP BY pname
     ORDER BY Tourneys DESC;
 
-
 --Rank characters by total times played in Frostbite 2019.
 SELECT Character, sum(Play_rate) as Total_Played
 FROM (SELECT char1 as Character, COUNT(*) as Play_rate
@@ -19,7 +18,27 @@ SELECT char2 as Character, COUNT(*) as Play_rate
 GROUP BY Character
 ORDER BY Total_Played DESC;
 
---List players by number of set wins.
+--Rank players by number of set wins for a given tournament
+SELECT Plyr, IFNULL(Set_wins,0) as Set_wins
+FROM (SELECT Plyr
+		FROM (SELECT plyr1 as Plyr
+				FROM SetSingles NATURAL JOIN Tournament
+				WHERE tname = "Frostbite 2019"
+				GROUP BY plyr1
+			  UNION
+			  SELECT plyr2 as Plyr
+				FROM SetSingles NATURAL JOIN Tournament
+				WHERE tname = "Frostbite 2019"
+				GROUP BY plyr2
+			  )
+	  )
+LEFT JOIN
+	(SELECT swinner, COUNT(*) as Set_wins
+		FROM SetSingles NATURAL JOIN Tournament
+		WHERE tname = "Frostbite 2019"
+		GROUP BY swinner) on P lyr = swinner
+ORDER BY Set_wins DESC, Plyr ASC;
+
 --List players by number of fight wins.
 --List region by number of players from that region.
 --List region by number of tourneys in that region.
