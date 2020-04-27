32970300eb1728303e667c620ac834163feb2795
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2020-04-26 19:27:38 -0500

    Fixed disadvantage query

--- queries.sql
+++ queries.sql
@@ -70,17 +70,25 @@ Select plyr2 as Player, MIN(depth) as Round_Eliminated
     GROUP BY Player
     ORDER BY Round_Eliminated;
 
---Rank players by the number of fights won when their character is at disadvantage?
-SELECT plyr1 as Player, COUNT(*) as "Wins At Disadvantage"
-FROM(SELECT char1,char2,fwinner,plyr1,plyr2 FROM FightSingles NATURAL JOIN SetSingles)
-WHERE ((SELECT tierid FROM Character WHERE cname = char1) - (SELECT tierid FROM Character WHERE cname = char2)) > 0
-GROUP BY plyr1
-UNION
-SELECT plyr2 as Player, COUNT(*) as WinsAtDis
-FROM(SELECT char1,char2,fwinner,plyr1,plyr2 FROM FightSingles NATURAL JOIN SetSingles)
-WHERE ((SELECT tierid FROM Character WHERE cname = char1) - (SELECT tierid FROM Character WHERE cname = char2)) < 0
-GROUP BY plyr2
-ORDER BY WinsAtDis DESC;
+--Rank players by the number of fights won when their character is at disadvantage.
+SELECT Player,SUM(Wins_At_Disadvantage) as Wins_At_Disadvantage
+    FROM (SELECT plyr1 as Player, COUNT() as Wins_At_Disadvantage
+            FROM(SELECT char1,char2,fwinner,plyr1,plyr2
+                    FROM FightSingles NATURAL JOIN SetSingles)
+            WHERE (SELECT tratio
+                    FROM Matchup
+                    WHERE c1name = char1 AND c2name = char2) > 1 AND char1 = fwinner
+            GROUP BY plyr1
+        UNION
+        SELECT plyr2 as Player, COUNT() as Wins_At_Disadvantage
+            FROM(SELECT char1,char2,fwinner,plyr1,plyr2
+                    FROM FightSingles NATURAL JOIN SetSingles)
+            WHERE (SELECT tratio
+                    FROM Matchup
+                    WHERE c1name = char2 AND c2name = char1) > 1 AND char2 = fwinner
+            GROUP BY plyr2)
+    GROUP BY Player
+    ORDER BY Wins_At_Disadvantage DESC
 
 --Which character performs the best against others on average?
 
